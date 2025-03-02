3ffd36ac9377a3ef3f0574d3d3209f143a0b836d
Alexandr <a9822496044@gemail.com>
2025-03-02 16:13:21 +0300

    Implemented the Cat class. Unit testing has been performed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHelloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHelloWorld1);
+//        System.out.println(beanHelloWorld.getMessage());
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld2);
+//        System.out.println(beanHelloWorld.getMessage());
+        System.out.println(beanHelloWorld1.equals(beanHelloWorld2));
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1);
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2);
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
